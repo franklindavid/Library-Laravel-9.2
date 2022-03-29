63d3051142b828117caaa06d47706370328aadf9
franklin de jesus david rodriguez <franklindavid91@gmail.com>
2022-03-29 10:57:25 -0500

    fixing bug sweetalert2

--- config/adminlte.php
+++ config/adminlte.php
@@ -405,7 +405,7 @@ return [
             ],
         ],
         'Sweetalert2' => [
-            'active' => false,
+            'active' => true,
             'files' => [
                 [
                     'type' => 'js',
