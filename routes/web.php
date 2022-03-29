08ae46223ca43fb7f249b19f182defcbf1d561b5
franklin de jesus david rodriguez <franklindavid91@gmail.com>
2022-03-29 16:28:19 -0500

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -22,12 +22,7 @@ Auth::routes();
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home')->middleware('auth');
 Route::resource('books',BooksController::class)->middleware('auth');
 Route::controller(BooksController::class)->group(function () {
-//     Route::get('/books/{id}', 'show')->name('books.show');
-//     Route::post('/books', 'store')->name('books.store');
-//     Route::get('/books', 'index')->name('books.index');
-//     Route::get('/books/{id}/edit', 'edit')->name('books.edit');
     Route::get('/books/{id}/delete', 'delete')->name('books.delete')->middleware('auth');
     Route::get('/books/{id}/request', 'request')->name('books.request')->middleware('auth');
     Route::get('/books/{id}/return', 'return')->name('books.return')->middleware('auth');
-//     Route::get('/books/create', 'create')->name('books.create');
 });
